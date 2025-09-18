3051bc91b24c91b4753a07412d8c2bb8d0008e12
nachum <nahum0583@gmail.com>
2025-09-18 10:28:25 +0300

    apdate match

--- services/indexer/elastic_service.py
+++ services/indexer/elastic_service.py
@@ -16,27 +16,56 @@ class ElasticService:
             refresh=refresh
         )
 
-    def match_search(self,doc_id, size:int = 1):
-
+    def match_search(self, doc_id, size: int = 1, filters: dict = None):
         doc = self.es.get_doc(doc_id)
         if not doc:
             logger.warning(f"No document found for id {doc_id} in index {self.index}")
             return
+
         index_gender = "female" if self.index == "male" else "male"
 
         text_self_vector = doc["text_self_vector"]
         text_for_search_vector = doc["text_for_search_vector"]
 
+        base_query = {"match_all": {}}
+
+        if filters:
+            must_filters = []
+            must_not_filters = []
+
+            # must
+            for field, value in filters.get("include", {}).items():
+                if isinstance(value, list):
+                    must_filters.append({"terms": {field: value}})
+                elif isinstance(value, dict): # range
+                    must_filters.append({"range": {field: value}})
+                else:
+                    must_filters.append({"term": {field: value}})
+
+            # not_must
+            for field, value in filters.get("exclude", {}).items():
+                if isinstance(value, list):
+                    must_not_filters.append({"terms": {field: value}})
+                elif isinstance(value, dict): # range
+                    must_not_filters.append({"range": {field: value}})
+                else:
+                    must_not_filters.append({"term": {field: value}})
+
+            base_query = {
+                "bool": {
+                    "filter": must_filters,
+                    "must_not": must_not_filters
+                }
+            }
+
         resp = self.es.search(
             index=index_gender,
             query={
                 "script_score": {
-                    "query": {"match_all": {}},
+                    "query": base_query,
                     "script": {
                         "source": """
-                            // self-text similarity versus text search
                             double score1 = cosineSimilarity(params.query_offer_vector, 'text_for_search_vector');
-                            // text search similarity versus self-text
                             double score2 = cosineSimilarity(params.query_search_vector, 'text_self_vector');
                             return score1 + score2 + 1.0;
                         """,
@@ -50,8 +79,5 @@ class ElasticService:
             size=size
         )
 
-        list_profile_id =[]
-        for hit in resp["hits"]["hits"]:
-            list_profile_id.append(hit['_id'])
-
+        list_profile_id = [hit["_id"] for hit in resp["hits"]["hits"]]
         return list_profile_id
